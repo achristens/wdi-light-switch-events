6e1570f258f9feef6f8fe64e5b241d7a0825c026
Abby Christens <achristens@gmail.com>
2017-11-13 13:54:14 -0500

    Change status on click

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
 // DOMContentLoaded is triggered by the browser when the DOM has been completely loaded:
 document.addEventListener('DOMContentLoaded', function() {
   var lightSwitch = document.querySelector('button');
-  var body = document.querySelector('body');
+  var body        = document.querySelector('body');
+  var status      = document.querySelector('h1');
   lightSwitch.addEventListener('click', function(){
       lightSwitch.classList.toggle('off');
       body.classList.toggle('dark');
+      if (lightSwitch.classList.contains('off') ){
+      status.innerHTML = "Hey, who turned off the lights?";
+    } else {
+      status.innerHTML = "It's so bright in here!";
+    }
   });
 
 });
