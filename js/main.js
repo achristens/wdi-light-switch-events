f0cb7e14f8a3e03063f5d42a857eb6255cb29365
Abby Christens <achristens@gmail.com>
2017-11-13 13:49:04 -0500

    Toggle background colour

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 // DOMContentLoaded is triggered by the browser when the DOM has been completely loaded:
 document.addEventListener('DOMContentLoaded', function() {
-  var lightSwitch = document.querySelector('button')
+  var lightSwitch = document.querySelector('button');
+  var body = document.querySelector('body');
   lightSwitch.addEventListener('click', function(){
       lightSwitch.classList.toggle('off');
-
+      body.classList.toggle('dark');
   });
 
 });
