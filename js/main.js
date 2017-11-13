a92c49bde7d9ecd3c995ff6921013f046460c971
Abby Christens <achristens@gmail.com>
2017-11-13 13:46:42 -0500

    Toggle on/off class

--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
+// DOMContentLoaded is triggered by the browser when the DOM has been completely loaded:
+document.addEventListener('DOMContentLoaded', function() {
+  var lightSwitch = document.querySelector('button')
+  lightSwitch.addEventListener('click', function(){
+      lightSwitch.classList.toggle('off');
 
+  });
 
+});
